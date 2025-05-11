9e5d43a7b683ae5cd6234bc622093679ced94104
muhafifs <mafifsiduppa@mail.ugm.ac.id>
2025-05-11 22:34:22 +0700

    testing

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -1,3 +1,5 @@
+import sys
+
 MENU = {
     "espresso": {
         "ingredients": {
@@ -25,16 +27,39 @@ MENU = {
 }
 
 resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
+    "Water": 300,
+    "Milk": 200,
+    "Coffee": 100,
+    "Money": 0,
 }
 
-money = 0
+coin = {
+    "penny": 0.01,
+    "nickel": 0.05,
+    "dime": 0.10,
+    "quarter": 0.25,
+}
+# TODO 1: Ask for user input about coffee choice
+operate = False
+while not operate:
+    order = input("What would you like? (espresso/latte/cappuccino): ").strip().lower()
+    # Turning off machine with command from maintainer
+    if order == "off":
+        sys.exit("Turning off")
+    elif order == "report":
+        # printing resources but adjusting for
+        for resource in resources:
+            if resource == "Water" or resource == "Milk":
+                print(f"{resource}: {resources[resource]}ml")
+            elif resource == "Coffee":
+                print(f"{resource}: {resources[resource]}g")
+            else:
+                print(f"{resource}: ${resources[resource]}")
 
+    if order == "report":
+        continue
 
-penny = 0.01
-nickel = 0.05
-dime = 0.10
-quarter = 0.25
+    for check_resource in MENU:
+        if
+
 
