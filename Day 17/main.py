42980d7f6d4f0d8ed8ccdc3373721ded3d2ba7e1
muhafifs <mafifsiduppa@mail.ugm.ac.id>
2025-05-14 14:54:07 +0700

    day 17

---
+++ Day 17/main.py
@@ -0,0 +1,19 @@
+
+class Car:
+    def __init__(self, seats, tires):
+        self.seats = seats
+        self.tires = tires
+
+        print("new user being created....")
+
+toyota = Car(5, 4)
+
+print(toyota.seats)
+
+
+# user_1 = User()
+
+# user_1.id = "001"
+# user_1.username = "shiro"
+
+# print(user_1.username)
